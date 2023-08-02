90210419969fceede2165d0ac197397c19558804
Christian Halim <chris.halim@mobilepulsa.com>
2023-08-02 23:34:55 +0700

    MANTAP BANG MESSI

--- database/migrations/2023_08_02_195634_create_log_invoices_table.php
+++ database/migrations/2023_08_02_195634_create_log_invoices_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('log_invoices', function (Blueprint $table) {
-            $table->bigInteger('id_invoice')->primary();
+            $table->increments('id_invoice');
             $table->string('nomor_invoice');
             $table->string('nama_invoice');
             $table->string('asal_transaksi');
             $table->string('contact_number');
             $table->string('address_invoice');
-            $table->integer('total_transaksi',50);
-            $table->unsignedBigInteger('id_user');             
+            $table->unsignedInteger('total_transaksi');
+            $table->unsignedInteger('id_user');
             $table->foreign('id_user')->references('id_user')->on('users');
             $table->timestamps();
         });
